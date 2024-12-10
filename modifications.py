728d131aa9e1b6d331af6a5a405d3b98007665cd
Oglogot <adalatyp@gmail.com>
2024-12-10 16:43:13 +0300

    Add files via upload

---
+++ modifications.py
@@ -0,0 +1,34 @@
+modifications_dict = {
+    'f' : "2'-fluoro",
+    '1t' : "3'-monothio",
+    '2t' : "3'-dithio",
+    'tp' : "3'-thiophosphoryl", # 'ps' : "phosphorothioate"
+    'omtri' : "3'-O-azidomethyl",
+    'idt' : "inverted 3'-thymine", #имеет смысл заменить на iT
+    'i' : "inverted",
+    'chol' : "cholesterol", #на 5' сажаются
+    'gpeg' : "galactose-polyethylene glycol",
+    'peg10' : "polyethylene glycol 10 kDa",
+    'peg20' : "polyethylene glycol 20 kDa",
+    'peg40' : "polyethylene glycol 40 kDa",
+    'papeg20' : "polyamine-polyethylene glycol 20 kDa",
+    'papeg40' : "polyamine-polyethylene glycol 40 kDa",
+    'halpeg' : "hexilamine linker from the picture with polyethylene glycol",
+    'hegl' : "hexaethylene glycol linker",
+    'mp' : "methylphosphonate",
+    'ome' : "2'-methoxy group",
+    'c3' : "3' alkyl linker in place of the nucleoside", #алкилы разные бывают? Какой тут - неясно (https://doi.org/10.1074/jbc.M113.532580). Или помечать как L?
+    'bn' : "5-(N-benzylcarboxamide)-2'-deoxyuridine",
+    'pe' : "5-[N-(1-naphthylmethyl)carboxamide]-2'-deoxyuridine",
+    'al' : "2'-allyl",
+    'd' : "chimeric deoxyribonucleotide",
+    'r' : "chimeric ribonucleotide",
+    'ch2' : "2'-methylene",
+    'am' : "2'-amino",
+    'ana' : "2'F-ANA", # 'af' : "2'-arabinofluoro" 
+    '2fc' : "2'-difluoromethylene",
+    'dspe' : "distearol-phosphotidylethanol",
+    'dsa' : "docosanamine",
+    'thiol' : "5' thiol",
+    'lna' : "locked nucleic acid"
+}
