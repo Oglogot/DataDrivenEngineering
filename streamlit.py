bf7f11fdbd91b5a85c4d09ecb5fba6a5cdb4f357
Oglogot <adalatyp@gmail.com>
2025-01-12 22:39:03 +0000

    Streamlit done

--- streamlit.py
+++ streamlit.py
@@ -49,18 +49,100 @@ def plot_bar(df, column, title, colors):
     st.plotly_chart(fig)
 
 
-# Анализ
-
-    # Распределения по категориям
-
+# Загрузка датасета
 relative_path = 'data/AptamerModifDataCleaned.csv'
 df = pd.read_csv(relative_path)
 
+# Анализ
+
+    # Метрики датасета
+st.header("На данный момент в датасете собрано:")
+
+num_rows = len(df)
+num_unique_doi = df["DOI"].nunique()
+
+# Using st.cache_data as recommended in the previous response
+@st.cache_data
+def load_modifications():
+    from modifications import modifications
+    return modifications
+
+modifications = load_modifications()
+
+num_modifications = len(modifications)
+
+
+col1, col2 = st.columns(2)  # Разделение на две колонки
+
+with col1:
+    st.metric("Последовательностей", num_rows)
+
+
+with col2:
+    st.metric("Модификаций", num_modifications)
+
+st.subheader("Статей обработано:")
+st.metric("", num_unique_doi)
+
+
+# Подсчет собранного количества модификаций
+
+st.subheader("Собрано модификаций каждого вида")
+
+from modifications import modifications
+# Регулярное выражение для поиска модификаций
+# Мы используем позиционный захват, чтобы найти модификации, которые не являются префиксами других модификаций
+modification_pattern = re.compile('|'.join(map(re.escape, modifications.keys())))
+# Словарь для подсчета модификаций
+modification_count = defaultdict(int)
+# Проход по каждой строке в датасете
+for seq in df['Sequence']:
+    # Нахождение всех модификаций в строке
+    found_modifications = modification_pattern.findall(seq)
+    # Множество для хранения уникальных модификаций в этой строке
+    unique_modifications = set()
+    for mod in found_modifications:
+        # Проверяем, добавлена ли модификация в множество
+        if mod not in unique_modifications:
+            # Если нет, добавляем её и увеличиваем счетчик модификации
+            unique_modifications.add(mod)
+            modification_count[mod] += 1
+# Вывод результатов
+for mod, count in modification_count.items():
+    print(f"{modifications[mod]}: {count}")
+    modification_df = pd.DataFrame.from_dict(modification_count, orient='index', columns=['Count'])
+modification_df = modification_df.reset_index()
+modification_df['Modification'] = modification_df['index'].map(modifications)
+
+# Создание столбчатой диаграммы
+fig = px.bar(
+    modification_df, 
+    x='Modification', 
+    y='Count', 
+    color='Modification', 
+    color_discrete_sequence=colors, 
+    labels={'Modification': 'Вид модификации', 'Count': 'Количество вхождений'},
+    text='Count'  # Добавляем значения на столбцы
+)
+fig.update_layout(
+    showlegend=False,    
+    height=800,  width=1400
+)
+fig.update_traces(textposition='outside')
+# Отображение графика
+st.plotly_chart(fig)
+
+    # Распределения по категориям
+
+st.header("Распределения данных по категориям")
+
+plot_pie(df, 'Modified', "Соотношение модифицированных и немодифицированных последовательностей", colors)
 plot_bar(df, 'Environment', "Среды, использовавшиеся для определения стабильности", colors)
 plot_bar(df, 'Type', "Типы последовательностей, представленных в датасете", colors)
-plot_pie(df, 'Modified', "Соотношение модифицированных и немодифицированных последовательностей", colors)
 
     # Распределения целевой величины
+
+st.header("Распределения значений периода полураспада")
 
 fig = make_subplots(rows=1, cols=2, subplot_titles=("Гистограмма", "Violin Plot"), column_widths=[0.5, 0.5])
 # Добавление гистограммы
@@ -73,7 +155,7 @@ violin_fig = px.violin(df, y="Stability t1/2, h",
         color_discrete_sequence=colors)
 fig.add_trace(violin_fig['data'][0], row=1, col=2)
 # Настройка макета
-fig.update_layout(showlegend=True, title_text="Распределение значений периода полураспада", height=600,  width=1200) 
+fig.update_layout(showlegend=True, title_text="Распределение значений периода полураспада в целом", height=600,  width=1200) 
 st.plotly_chart(fig)
 
 # Создание подграфиков
@@ -142,48 +224,3 @@ fig = px.scatter(df, x="Concentration of envi, %", y="Stability t1/2, h", color=
 st.plotly_chart(fig)
 
 
-# Подсчет собранного количества модификаций
-
-from modifications import modifications
-# Регулярное выражение для поиска модификаций
-# Мы используем позиционный захват, чтобы найти модификации, которые не являются префиксами других модификаций
-modification_pattern = re.compile('|'.join(map(re.escape, modifications.keys())))
-# Словарь для подсчета модификаций
-modification_count = defaultdict(int)
-# Проход по каждой строке в датасете
-for seq in df['Sequence']:
-    # Нахождение всех модификаций в строке
-    found_modifications = modification_pattern.findall(seq)
-    # Множество для хранения уникальных модификаций в этой строке
-    unique_modifications = set()
-    for mod in found_modifications:
-        # Проверяем, добавлена ли модификация в множество
-        if mod not in unique_modifications:
-            # Если нет, добавляем её и увеличиваем счетчик модификации
-            unique_modifications.add(mod)
-            modification_count[mod] += 1
-# Вывод результатов
-for mod, count in modification_count.items():
-    print(f"{modifications[mod]}: {count}")
-    modification_df = pd.DataFrame.from_dict(modification_count, orient='index', columns=['Count'])
-modification_df = modification_df.reset_index()
-modification_df['Modification'] = modification_df['index'].map(modifications)
-
-# Создание столбчатой диаграммы
-fig = px.bar(
-    modification_df, 
-    x='Modification', 
-    y='Count', 
-    color='Modification', 
-    color_discrete_sequence=colors, 
-    title='Количество модификаций каждого вида',
-    labels={'Modification': 'Вид модификации', 'Count': 'Количество вхождений'},
-    text='Count'  # Добавляем значения на столбцы
-)
-fig.update_layout(
-    showlegend=False,    
-    height=600,  width=1200
-)
-fig.update_traces(textposition='outside')
-# Отображение графика
-st.plotly_chart(fig)
